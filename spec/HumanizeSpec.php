4fc93c63b2352ca38f2930e1ad2e54daab692627
Mark Hanson <mhanson01@gmail.com>
2014-09-17 00:50:56 -0400

    some of the basics up and running

--- spec/HumanizeSpec.php
+++ spec/HumanizeSpec.php
@@ -11,4 +11,170 @@ class HumanizeSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Humanize');
     }
+
+    function it_formats_numbers_with_commas()
+    {
+        $this->intcomma(1000)->shouldReturn('1,000');
+    }
+
+    function it_formats_large_numbers_with_commas()
+    {
+        $this->intcomma(1000000000)->shouldReturn('1,000,000,000');
+    }
+
+    function it_converts_number_less_than_10_to_words()
+    {
+        $this->apnumber(1)->shouldReturn('one');
+    }
+
+    function it_converts_2_to_two()
+    {
+        $this->apnumber(2)->shouldReturn('two');
+    }
+
+    function it_does_not_convert_numbers_10_or_larger_to_words()
+    {
+        $this->apnumber(10)->shouldReturn(10);
+    }
+
+    function it_returns_today_for_the_current_date()
+    {
+        $this->naturalday(date('m/d/Y'))->shouldReturn('today');
+    }
+
+    function it_returns_yesterday_for_yesterdays_date()
+    {
+        $this->naturalday(date('m/d/Y', strtotime('-1 day')))->shouldReturn('yesterday');
+    }
+
+    function it_returns_tomorrow_for_tomorrows_date()
+    {
+        $this->naturalday(date('m/d/Y', strtotime('+1 day')))->shouldReturn('tomorrow');
+    }
+
+    function it_returns_the_date_when_its_farther_than_one_day_away()
+    {
+        $this->naturalday('12/31/2000')->shouldReturn('12/31/2000');
+    }
+
+    function it_formats_the_date_when_supplied_with_date_format()
+    {
+        $this->naturalday('12/31/2000', 'Y-m-d')->shouldReturn('2000-12-31');
+    }
+
+    function it_adds_the_ordinal_suffix_to_a_number()
+    {
+        $this->ordinal(1)->shouldReturn('1st');
+    }
+
+    function it_adds_the_ordinal_suffix_to_2()
+    {
+        $this->ordinal(2)->shouldReturn('2nd');
+    }
+
+    function it_adds_the_ordinal_suffix_to_3()
+    {
+        $this->ordinal(3)->shouldReturn('3rd');
+    }
+
+    function it_adds_the_ordinal_suffix_to_4()
+    {
+        $this->ordinal(4)->shouldReturn('4th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_5()
+    {
+        $this->ordinal(5)->shouldReturn('5th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_6()
+    {
+        $this->ordinal(6)->shouldReturn('6th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_7()
+    {
+        $this->ordinal(7)->shouldReturn('7th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_8()
+    {
+        $this->ordinal(8)->shouldReturn('8th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_9()
+    {
+        $this->ordinal(9)->shouldReturn('9th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_10()
+    {
+        $this->ordinal(10)->shouldReturn('10th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_11()
+    {
+        $this->ordinal(11)->shouldReturn('11th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_12()
+    {
+        $this->ordinal(12)->shouldReturn('12th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_13()
+    {
+        $this->ordinal(13)->shouldReturn('13th');
+    }
+
+    function it_adds_the_ordinal_suffix_to_23()
+    {
+        $this->ordinal(23)->shouldReturn('23rd');
+    }
+
+    function it_adds_the_ordinal_suffix_to_52()
+    {
+        $this->ordinal(52)->shouldReturn('52nd');
+    }
+
+    function it_convert_large_integers_to_words()
+    {
+        $this->intword(1000000)->shouldReturn('1 million');
+    }
+
+    function it_converts_a_billion_to_words()
+    {
+        $this->intword(1000000000)->shouldReturn('1 billion');
+    }
+
+    function it_converts_123456789_to_words()
+    {
+        $this->intword(123456789)->shouldReturn('123 million');
+    }
+
+    function it_formats_numbers_with_decimal_places_and_commas()
+    {
+        $this->formatnumbers(123456789, 2)->shouldReturn('123,456,789.00');
+    }
+
+    function it_formats_numbers_that_have_decimals_already()
+    {
+        $this->formatnumbers(123456789.12, 2)->shouldReturn('123,456,789.12');
+    }
+
+    function it_compacts_large_numbers()
+    {
+        $this->compactinteger(123456789, 1)->shouldReturn('123.5M');
+    }
+
+    function it_bounds_a_number_to_the_max()
+    {
+        $this->boundednumber(110, 100)->shouldReturn('100+');
+    }
+
+    function it_allows_a_number_within_the_bounds()
+    {
+        $this->boundednumber(50, 100)->shouldReturn(50);
+    }
+
 }
--- src/Humanize.php
+++ src/Humanize.php
@@ -2,5 +2,120 @@
 
 class Humanize
 {
-    
+    protected $magnitudes = array(
+        12 => 'trillion',
+        9  => 'billion',
+        6  => 'million',
+        3  => 'thousand',
+        0  => ''
+    );
+    protected $abbreviations = array(
+        12 => 'T',
+        9  => 'B',
+        6  => 'M',
+        3  => 'K',
+        0  => ''
+    );
+
+    public function intcomma($int)
+    {
+        return number_format($int, 0, null, ',');
+    }
+
+    public function intword($int, $decimal_places = 0, $type = 'word')
+    {
+        if($type == 'compact')
+        {
+            $array = $this->abbreviations;
+            $spacer = null;
+        }
+        else
+        {
+            $array = $this->magnitudes;
+            $spacer = ' ';
+        }
+
+        foreach($array as $exponent => $suffix)
+        {
+            if($int >= pow(10, $exponent))
+            {
+                return round(floatval($int / pow(10, $exponent)), $decimal_places) . $spacer . $suffix;
+            }
+        }
+
+        return $int;
+    }
+
+    public function apnumber($int)
+    {
+        $dictionary = array(
+            0 => 'zero',
+            1 => 'one',
+            2 => 'two',
+            3 => 'three',
+            4 => 'four',
+            5 => 'five',
+            6 => 'six',
+            7 => 'seven',
+            8 => 'eight',
+            9 => 'nine',
+        );
+
+        return isset($dictionary[$int]) ? $dictionary[$int] : $int;
+    }
+
+    public function naturalday($date, $format = 'm/d/Y')
+    {
+        $date = date($format, strtotime($date));
+
+        if($date == $this->yesterday($format)) return 'yesterday';
+
+        if($date == $this->tomorrow($format)) return 'tomorrow';
+
+        if($date == $this->today($format)) return 'today';
+
+        return $date;
+    }
+
+    public function yesterday($format = 'm/d/Y')
+    {
+        return date($format, strtotime('-1 day'));
+    }
+
+    public function tomorrow($format = 'm/d/Y')
+    {
+        return date($format, strtotime('+1 day'));
+    }
+
+    public function today($format = 'm/d/Y')
+    {
+        return date($format);
+    }
+
+    public function ordinal($number)
+    {
+        $ends = array('th','st','nd','rd','th','th','th','th','th','th');
+
+        $mod100 = $number % 100;
+
+        return $number . ($mod100 >= 11 && $mod100 <= 13 ? 'th' :  $ends[$number % 10]);
+    }
+
+    public function formatnumbers($number)
+    {
+        return number_format($number, 2, '.', ',');
+    }
+
+    public function compactinteger($int, $decimal_places = 0)
+    {
+        return $this->intword($int, $decimal_places, 'compact');
+    }
+
+    public function boundednumber($value, $max)
+    {
+        if($value > $max) return $max . '+';
+
+        return $value;
+    }
+
 }
